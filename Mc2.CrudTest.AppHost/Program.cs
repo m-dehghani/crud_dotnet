07fce317030d82c65bb38fb5f1d656d0c86fdc01
Mohammad Dehghani <dehghany.m@gmail.com>
2024-09-12 04:38:21 +0330

    add kafka to the pipeline for event streaming and event sourcing

--- Mc2.CrudTest.AppHost/Program.cs
+++ Mc2.CrudTest.AppHost/Program.cs
@@ -1,13 +1,28 @@
-IDistributedApplicationBuilder? builder = DistributedApplication.CreateBuilder(args);
+
+IDistributedApplicationBuilder? builder = 
+       DistributedApplication.CreateBuilder(args);
+
+IResourceBuilder<RedisResource>? redis = 
+       builder.AddRedis("cache");
+
+IResourceBuilder<ParameterResource> username = builder.AddParameter("username", secret: true);
+IResourceBuilder<ParameterResource> password = builder.AddParameter("password", secret: true);
+
+IResourceBuilder<PostgresServerResource>? postgres = 
+       builder.AddPostgres("NpgsqlConnection").WithPgWeb();
+
+IResourceBuilder<PostgresDatabaseResource>? postgresdb = 
+       postgres.AddDatabase("customers");
+
+var messaging = builder.AddKafka("messaging")
+                       .WithKafkaUI();
 
 
-IResourceBuilder<RedisResource>? redis = builder.AddRedis("cache");
-
-IResourceBuilder<PostgresServerResource>? postgres = builder.AddPostgres("NpgsqlConnection");
-IResourceBuilder<PostgresDatabaseResource>? postgresdb = postgres.AddDatabase("customers");
-
-builder.AddProject<Projects.Mc2_CrudTest_Presentation_Server>("mc2-crudtest-presentation-server").WithReference(redis).WithReference(postgresdb).WithHttpEndpoint(port: 5066)
-       .WithHttpsEndpoint(port: 7239); ; ;
-
+builder.AddProject<Projects.Mc2_CrudTest_Presentation_Server>
+              ("mc2-crudtest-presentation-server")
+       .WithReference(redis)
+       .WithReference(postgresdb).WithReference(messaging);
+       .WithHttpEndpoint(port: 5066);
+       .WithHttpsEndpoint(port: 7239);
 
 builder.Build().Run();
